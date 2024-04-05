6c4174d852b6cc0173d356afe3792f7edcfcc5fc
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-05 14:30:52 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,5 @@
-Exit
+# Specify the YouTube link
+$youtubeLink = "https://youtu.be/dQw4w9WgXcQ?si=CZkDDWPqwej3_twg"
+
+# Command to open Chrome with the YouTube link
+Start-Process "chrome.exe" -ArgumentList $youtubeLink
