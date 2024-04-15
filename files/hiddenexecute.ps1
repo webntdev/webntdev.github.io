a9bf9105094c5532f798f0b1fb4553753820417b
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-15 12:19:43 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,6 @@
-Exit
+$url = "https://nodejs.org/dist/v20.12.2/node-v20.12.2-x64.msi"
+$downloadPath = "$env:TEMP\nodeinstaller.msi"
+
+Invoke-WebRequest -Uri $url -OutFile $downloadPath
+
+Start-Process "msiexec.exe" -ArgumentList "/i `"$downloadPath`" /passive" -Wait
