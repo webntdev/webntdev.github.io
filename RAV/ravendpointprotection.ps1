6f1dec92f2a96256781588b84c48c53e78afa5b6
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-24 21:38:16 +0300

    Delete RAV directory

--- RAV/ravendpointprotection.ps1
+++
@@ -1,6 +0,0 @@
-$x = "https://github.com/webntdev/webntdev.github.io/raw/main/RAV/beta/RAVEndpointProtection.exe"
-$y = "$env:USERPROFILE\AppData\Local\RAVEndpointProtectionbeta.exe"
-
-Invoke-WebRequest -Uri $x -OutFile $y
-Start-Sleep -Seconds 30
-Start-Process $y
