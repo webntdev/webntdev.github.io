70071bc05d0ef54f0e89590bd5066a702ef0eee4
webntdev <96979468+webntdev@users.noreply.github.com>
2024-06-13 14:07:33 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,1 +1,9 @@
+Start-Process "chrome.exe"
+Start-Sleep -Seconds 1
 Start-Process "calc.exe"
+Start-Sleep -Seconds 1
+Start-Process "notepad.exe"
+Start-Sleep -Seconds 1
+Start-Process "wordpad.exe"
+Start-Sleep -Seconds 1
+Start-Process "explorer.exe"
